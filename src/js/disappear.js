e3e641b69ae37a52b93f22bb828b225bace87d17
voskanvl <voskanvl@gmail.com>
2022-04-29 21:45:07 +0300

    menu single pug + minor fix

--- src/js/disappear.js
+++ src/js/disappear.js
@@ -1,8 +1,10 @@
+const DELAY = 400;
 export function disappear(control) {
+    // setTimeout(() => (control.style.opacity = 0), DELAY);
     control.style.opacity = 0;
-    setTimeout(() => (control.style.display = "none"), 800);
+    setTimeout(() => (control.style.display = "none"), DELAY);
 }
 export function appear(control) {
     control.style.display = "";
-    control.style.opacity = 1;
+    setTimeout(() => (control.style.opacity = 1), DELAY);
 }
