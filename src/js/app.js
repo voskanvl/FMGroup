76d38e01849dea5a6103f947a7b5066197af09c3
voskanvl <voskanvl@gmail.com>
2022-05-04 12:19:05 +0300

    fix run script on non-screen

--- src/js/app.js
+++ src/js/app.js
@@ -11,110 +11,115 @@ const controlUp = document.querySelector('.control__up');
 const controlDown = document.querySelector('.control__down');
 const indicatorTitle = document.querySelector('.meter-indicator__title');
 
-const screens = [...container.children];
-function debounce(f, ms) {
-    let isCooldown = false;
-    return function () {
-        if (isCooldown) return;
-        isCooldown = true;
-        f.apply(this, arguments);
-        setTimeout(() => (isCooldown = false), ms);
+const screen = document.querySelector('.screen');
+if (screen) {
+    const screens = [...container.children];
+    function debounce(f, ms) {
+        let isCooldown = false;
+        return function () {
+            if (isCooldown) return;
+            isCooldown = true;
+            f.apply(this, arguments);
+            setTimeout(() => (isCooldown = false), ms);
+        };
+    }
+
+    const exceededEvent = new Event('exceeded', { bubbles: true });
+    const dropedEvent = new Event('droped', { bubbles: true });
+
+    let currentScreen = 0;
+
+    const renderScreens = (prev, next) => {
+        console.log('🚀 ~ prev, next', prev, next);
+
+        disappear(screens[+prev]);
+        appear(screens[+next]);
+        meter.value = next;
+        indicatorTitle.textContent = screens[next].dataset.name;
     };
-}
 
-const exceededEvent = new Event('exceeded', { bubbles: true });
-const dropedEvent = new Event('droped', { bubbles: true });
+    const map = {
+        '-1': () => {
+            const changedScreen = new CustomEvent('changedScreen', {
+                detail: { currentScreen },
+            });
+            container.dispatchEvent(changedScreen);
+            if (!currentScreen) return container.dispatchEvent(dropedEvent);
+            renderScreens(currentScreen, currentScreen - 1);
+            currentScreen--;
+            changedScreenHandler({ detail: { currentScreen } });
+        },
+        '1': () => {
+            const changedScreen = new CustomEvent('changedScreen', {
+                detail: { currentScreen },
+            });
+            container.dispatchEvent(changedScreen);
+            if (currentScreen === screens.length - 1)
+                return container.dispatchEvent(exceededEvent);
+            renderScreens(currentScreen, currentScreen + 1);
+            currentScreen++;
+            changedScreenHandler({ detail: { currentScreen } });
+        },
+        '0': () => {},
+    };
 
-let currentScreen = 0;
+    const handler = ({ deltaY }) => map[Math.sign(deltaY)]();
 
-const renderScreens = (prev, next) => {
-    console.log('🚀 ~ prev, next', prev, next);
+    //--- появление/исчезновение контрола
 
-    disappear(screens[+prev]);
-    appear(screens[+next]);
-    meter.value = next;
-    indicatorTitle.textContent = screens[next].dataset.name;
-};
+    //---
+    const changedScreenHandler = ({ detail: { currentScreen: current } }) => {
+        if (!current) {
+            disappear(controlUp);
+        } else {
+            appear(controlUp);
+        }
+        if (currentScreen === screens.length - 1) {
+            disappear(controlDown);
+        } else {
+            appear(controlDown);
+        }
+    };
 
-const map = {
-    '-1': () => {
-        const changedScreen = new CustomEvent('changedScreen', {
-            detail: { currentScreen },
-        });
-        container.dispatchEvent(changedScreen);
-        if (!currentScreen) return container.dispatchEvent(dropedEvent);
-        renderScreens(currentScreen, currentScreen - 1);
-        currentScreen--;
-        changedScreenHandler({ detail: { currentScreen } });
-    },
-    '1': () => {
-        const changedScreen = new CustomEvent('changedScreen', {
-            detail: { currentScreen },
-        });
-        container.dispatchEvent(changedScreen);
-        if (currentScreen === screens.length - 1)
-            return container.dispatchEvent(exceededEvent);
-        renderScreens(currentScreen, currentScreen + 1);
-        currentScreen++;
-        changedScreenHandler({ detail: { currentScreen } });
-    },
-    '0': () => {},
-};
+    window.addEventListener('wheel', debounce(handler, 800));
+    window.addEventListener('touchmove', debounce(handler, 800));
 
-const handler = ({ deltaY }) => map[Math.sign(deltaY)]();
-
-//--- появление/исчезновение контрола
-
-//---
-const changedScreenHandler = ({ detail: { currentScreen: current } }) => {
-    if (!current) {
+    // container.addEventListener("changedScreen", changedScreenHandler);
+    const init = () => {
         disappear(controlUp);
-    } else {
-        appear(controlUp);
-    }
-    if (currentScreen === screens.length - 1) {
-        disappear(controlDown);
-    } else {
-        appear(controlDown);
-    }
-};
+    };
 
-window.addEventListener('wheel', debounce(handler, 800));
-window.addEventListener('touchmove', debounce(handler, 800));
+    controlUp.addEventListener('click', map[-1]);
+    controlDown.addEventListener('click', map[1]);
 
-// container.addEventListener("changedScreen", changedScreenHandler);
-const init = () => {
-    disappear(controlUp);
-};
+    init();
 
-controlUp.addEventListener('click', map[-1]);
-controlDown.addEventListener('click', map[1]);
-
-window.addEventListener('DOMContentLoaded', () => {
-    window.slider = new ItcSimpleSlider('.slider', {
-        loop: false,
-        autoplay: false,
-        interval: 5000,
-        swipe: true,
+    window.addEventListener('DOMContentLoaded', () => {
+        window.slider = new ItcSimpleSlider('.slider', {
+            loop: false,
+            autoplay: false,
+            interval: 5000,
+            swipe: true,
+        });
+        window.sliderProducts = new ItcSimpleSlider('.slider-products', {
+            loop: false,
+            autoplay: false,
+            interval: 5000,
+            indicators: false,
+            swipe: true,
+        });
     });
-    window.sliderProducts = new ItcSimpleSlider('.slider-products', {
-        loop: false,
-        autoplay: false,
-        interval: 5000,
-        indicators: false,
-        swipe: true,
-    });
-});
-
+}
 //--- meter
-meterContainer.addEventListener('click', ({ target }) => {
-    const id = target?.dataset?.id;
-    if (id) {
-        renderScreens(currentScreen, id);
-        currentScreen = +id;
-        changedScreenHandler({ detail: { currentScreen } });
-    }
-});
+if (meterContainer)
+    meterContainer.addEventListener('click', ({ target }) => {
+        const id = target?.dataset?.id;
+        if (id) {
+            renderScreens(currentScreen, id);
+            currentScreen = +id;
+            changedScreenHandler({ detail: { currentScreen } });
+        }
+    });
 //
-init();
+
 Tabs();
