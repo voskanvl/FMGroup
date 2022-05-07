1c168b41c412818eadbb04ebb29246e71c2e94aa
voskanvl <voskanvl@gmail.com>
2022-05-07 12:27:50 +0300

    fix remove listeners in tabs

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,27 +1,27 @@
 class Tabs {
     #listeners = [];
-    constructor(tabs = '.tabs') {
-        if (typeof tabs === 'string')
+    constructor(tabs = ".tabs") {
+        if (typeof tabs === "string")
             this.tabsElement = document.querySelector(tabs);
-        if (tabs.__proto__.constructor.name.includes('HTML'))
+        if (tabs.__proto__.constructor.name.includes("HTML"))
             this.tabsElement = tabs;
         this.tabs = this.tabsElement.children;
         this.currentTab = 0;
-        this.tabs[this.currentTab].classList.add('active');
+        this.tabs[this.currentTab].classList.add("active");
         this.handler = this.handler.bind(this);
-        this.tabsElement.addEventListener('click', this.handler);
+        this.tabsElement.addEventListener("click", this.handler);
     }
     addEventListener(listener) {
         this.#listeners.push(listener);
     }
     removeEventListener(listener) {
-        this.#listeners.push(listener);
+        this.#listeners = this.#listeners.filter(el => el === listener);
     }
     handler({ target }) {
-        const tab = target.closest('.tab');
+        const tab = target.closest(".tab");
         if (tab && tab.dataset.id) {
-            this.tabs[this.currentTab].classList.remove('active');
-            tab.classList.add('active');
+            this.tabs[this.currentTab].classList.remove("active");
+            tab.classList.add("active");
             this.currentTab = tab.dataset.id;
             if (this.#listeners) {
                 this.#listeners.forEach(cb => cb(this.currentTab));
