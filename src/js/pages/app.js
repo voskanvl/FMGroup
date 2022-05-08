25d8873c1703b45867ea1dbc43c354d2a07f9937
voskanvl <voskanvl@gmail.com>
2022-05-08 15:10:48 +0300

    touch events

--- src/js/pages/app.js
+++ src/js/pages/app.js
@@ -121,9 +121,30 @@ if (screen) {
             appear(controlDown);
         }
     };
-
+    let previousClientY = 0;
+    const handlerTouch = ev => {
+        ev.preventDefault();
+        const {
+            changedTouches: [{ clientY }],
+        } = ev;
+        const deltaY = previousClientY - clientY;
+        const signY = -Math.sign(deltaY);
+        console.log(signY);
+        previousClientY = clientY;
+        map[signY]();
+    };
     window.addEventListener("wheel", debounce(handler, 800));
-    window.addEventListener("touchmove", debounce(handler, 800));
+    window.addEventListener("touchstart", ev => {
+        const {
+            changedTouches: [{ clientY }],
+            target,
+        } = ev;
+        previousClientY = clientY;
+        target.click();
+    });
+    window.addEventListener("touchend", handlerTouch, {
+        passive: false,
+    });
 
     // container.addEventListener("changedScreen", changedScreenHandler);
     const init = () => {
