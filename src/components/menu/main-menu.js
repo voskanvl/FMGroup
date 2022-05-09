11a01d4f0ac0f9bce878a28842be35d103e931f5
voskanvl <voskanvl@gmail.com>
2022-05-09 11:24:35 +0300

    main-menu open fix

--- src/components/menu/main-menu.js
+++ src/components/menu/main-menu.js
@@ -1,27 +1,25 @@
-import { disappear, appear } from '../../js/disappear';
-const mainMenu = (element = '.main-menu') => {
+import { disappear, appear } from "../../js/disappear";
+const mainMenu = (element = ".main-menu") => {
     const menu = document.querySelector(element);
-    const label = document.querySelector(element + '__label');
-    const parent = menu.parentElement;
-    console.log('🚀 ~ parent', parent);
-
-    parent.addEventListener('click', ({ target }) => {
-        console.log('🚀 ~ target', target);
-        menu.classList.toggle('active');
-        const match = matchMedia('(max-width: 1024px)').matches;
-        console.log('🚀 ~ match', match);
-        const phone = document.querySelector('h1.header__phone');
-        if (match && menu.classList.contains('active')) {
-            phone.style.flex = '1 1 10%';
+    const label = document.querySelector(element + "__label");
+    const handler = ({ target }) => {
+        console.log("🚀 ~ target", target);
+        menu.classList.toggle("active");
+        const match = matchMedia("(max-width: 1024px)").matches;
+        console.log("🚀 ~ match", match);
+        const phone = document.querySelector("h1.header__phone");
+        if (match && menu.classList.contains("active")) {
+            phone.style.flex = "1 1 10%";
         } else {
-            phone.style.flex = '';
+            phone.style.flex = "";
         }
         //switch label
-        if (label && menu.classList.contains('active')) {
+        if (label && menu.classList.contains("active")) {
             disappear(label);
         } else {
             appear(label);
         }
-    });
+    };
+    menu.addEventListener("click", handler);
 };
 export default mainMenu;
