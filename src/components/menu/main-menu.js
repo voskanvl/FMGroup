c7c7e16db247adbc59a1969b9ade3803e6594ffc
voskanvl <voskanvl@gmail.com>
2022-05-10 10:06:08 +0300

    menu under 425

--- src/components/menu/main-menu.js
+++ src/components/menu/main-menu.js
@@ -2,12 +2,14 @@ import { disappear, appear } from "../../js/disappear";
 const mainMenu = (element = ".main-menu") => {
     const menu = document.querySelector(element);
     const label = document.querySelector(element + "__label");
+    const wrapper = document.querySelector(element + "__wrapper");
+    const ul = document.querySelector(element + "__wrapper > ul");
+    const phone = document.querySelector("h1.header__phone");
+
     const handler = ({ target }) => {
-        console.log("🚀 ~ target", target);
         menu.classList.toggle("active");
         const match = matchMedia("(max-width: 1024px)").matches;
-        console.log("🚀 ~ match", match);
-        const phone = document.querySelector("h1.header__phone");
+
         if (match && menu.classList.contains("active")) {
             phone.style.flex = "1 1 10%";
         } else {
