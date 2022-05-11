a2d1e9e67ebcd68e97f424fe825570fa144d5cbf
voskanvl <voskanvl@gmail.com>
2022-05-11 15:20:23 +0300

    switchCarousel move to MCarousel

--- src/js/MCarousel/initMCarousel.js
+++ src/js/MCarousel/initMCarousel.js
@@ -2,18 +2,17 @@ import { disappear, appear } from '../disappear';
 import recalculateCurrentPoints from './recalculateCurrentPoints';
 export default function initMCarousel(selector, productsHeaders, currentSlide) {
     const slider = document.querySelector(selector);
-    recalculateCurrentPoints(0, slider, 400);
+    recalculateCurrentPoints(0, slider, 800);
     M.Carousel.init(slider, {
         indicators: true,
         numVisible: 3,
         padding: 800,
-        shift: -50,
         onCycleTo({ dataset: { id } }) {
             if (id != currentSlide) {
                 disappear(productsHeaders[currentSlide]);
                 appear(productsHeaders[+id]);
                 currentSlide = +id;
-                recalculateCurrentPoints(id, slider, 400);
+                recalculateCurrentPoints(id, slider, 800);
             }
         },
     });
