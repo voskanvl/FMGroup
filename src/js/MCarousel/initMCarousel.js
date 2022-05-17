92c97e8f21b8fe34db213c9757d9e75dd9f2928a
Mike-05 <murodjontoshpulotovofficial@gmail.com>
2022-05-17 16:33:06 +0300

    change in MCarousel

--- src/js/MCarousel/initMCarousel.js
+++ src/js/MCarousel/initMCarousel.js
@@ -1,10 +1,10 @@
-import { disappear, appear } from '../disappear';
-import recalculateCurrentPoints from './recalculateCurrentPoints';
+import { disappear, appear } from "../disappear";
+import recalculateCurrentPoints from "./recalculateCurrentPoints";
 export default function initMCarousel(selector, productsHeaders, currentSlide) {
     const slider = document.querySelector(selector);
     recalculateCurrentPoints(0, slider, 800);
     M.Carousel.init(slider, {
-        indicators: true,
+        indicators: false,
         numVisible: 3,
         padding: 800,
         onCycleTo({ dataset: { id } }) {
