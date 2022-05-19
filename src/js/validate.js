29b3318a2c47f6cd1cc3e8779c9a6f98ee41ffee
Mike-05 <murodjontoshpulotovofficial@gmail.com>
2022-05-19 09:38:22 +0300

    validate fix styles

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,47 +1,49 @@
 import IMask from "imask";
-function showError(el) {
-    if (el.validity.valueMissing) {
-        // Если поле пустое,
-        // отображаем следующее сообщение об ошибке
-        emailError.textContent = "You need to enter an e-mail address.";
-    } else if (email.validity.typeMismatch) {
-        // Если поле содержит не email-адрес,
-        // отображаем следующее сообщение об ошибке
-        emailError.textContent = "Entered value needs to be an e-mail address.";
-    } else if (email.validity.tooShort) {
-        // Если содержимое слишком короткое,
-        // отображаем следующее сообщение об ошибке
-        emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
-    }
+// function showError(el) {
+//     if (el.validity.valueMissing) {
+//         // Если поле пустое,
+//         // отображаем следующее сообщение об ошибке
+//         emailError.textContent = "You need to enter an e-mail address.";
+//     } else if (email.validity.typeMismatch) {
+//         // Если поле содержит не email-адрес,
+//         // отображаем следующее сообщение об ошибке
+//         emailError.textContent = "Entered value needs to be an e-mail address.";
+//     } else if (email.validity.tooShort) {
+//         // Если содержимое слишком короткое,
+//         // отображаем следующее сообщение об ошибке
+//         emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+//     }
 
-    // Задаём соответствующую стилизацию
-    emailError.className = "error active";
-}
-function validateField(el, type) {
-    console.log("🚀 ~ el, type", el.validity, el.validationMessage);
-}
+//     // Задаём соответствующую стилизацию
+//     emailError.className = "error active";
+// }
+const validators = {
+    text() {
+        return;
+    },
+};
+// function validateField({ value }, type) {
+//     console.log(" validateField({value}, type)", value, type);
+// }
 export default function validate(form) {
-    const textInput = form.querySelectorAll('input[type="text"]');
+    // const textInput = form.querySelectorAll('input[type="text"]');
     const phoneInput = form.querySelectorAll('input[type="phone"]');
     const emailInput = form.querySelectorAll('input[type="email"]');
 
-    textInput.forEach(el =>
-        el.addEventListener("input", () => validateField(el, "text")),
-    );
-    const maskedPhoneInput = [...phoneInput].map(el => {
-        // el.addEventListener("input", () => validateField(el, "phone"));
+    // textInput.forEach(el =>
+    //     el.addEventListener("input", () => validateField(el, "text")),
+    // );
+    phoneInput.forEach(el => {
         return new IMask(el, {
             mask: "+{7}(000)000-00-00",
             validate(value, masked) {
-                console.log("phone (value, masked)", value, masked);
                 masked.rawInputValue.length === 10
                     ? el.classList.add("validated")
                     : el.classList.remove("validated");
             },
         });
     });
-    const maskedEmailInput = [...emailInput].map(el => {
-        // el.addEventListener("input", () => validateField(el, "phone"));
+    emailInput.forEach(el => {
         return new IMask(el, {
             mask: /.+/,
             doValidate() {
@@ -54,6 +56,4 @@ export default function validate(form) {
             },
         });
     });
-    console.log("🚀 ~ maskedEmailInput", maskedEmailInput);
-    // console.log("🚀 ~ maskedPhoneInput", maskedPhoneInput);
 }
