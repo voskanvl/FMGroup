5a16cd071342af9befde1dfedcc042cebefbc237
voskanvl <voskanvl@gmail.com>
2022-08-19 12:03:48 +0300

    minor fixes

--- src/js/MCarousel/initMCarousel.js
+++ src/js/MCarousel/initMCarousel.js
@@ -1,8 +1,6 @@
 import { disappear, appear } from "../disappear";
-import recalculateCurrentPoints from "./recalculateCurrentPoints";
 export default function initMCarousel(selector, productsHeaders, currentSlide) {
     const slider = document.querySelector(selector);
-    // recalculateCurrentPoints(0, slider, 800);
     M.Carousel.init(slider, {
         indicators: false,
         numVisible: 3,
